756f2aec0ffd353665734f5bbadc4f2b1acfad06
Bernard Bussy <bernard@andile.io>
2019-04-07 11:41:37 +0200

    remove unused server errors

--- server/exception/errors.go
+++ server/exception/errors.go
@@ -1,8 +1,6 @@
 package exception
 
 import (
-	"fmt"
-	serverMessage "gitlab.com/iotTracker/nerve/server/message"
 	"strings"
 )
 
@@ -20,46 +18,4 @@ type AcceptConnection struct {
 
 func (e AcceptConnection) Error() string {
 	return "accept connection error: " + strings.Join(e.Reasons, "; ")
-}
-
-type StartOfMessageNotFound struct {
-	Data    string
-	Reasons []string
-}
-
-func (e StartOfMessageNotFound) Error() string {
-	return fmt.Sprintf("start of message not found in given data:\n'%s'\n%s", e.Data, strings.Join(e.Reasons, "; "))
-}
-
-type DecodingError struct {
-	Reasons []string
-}
-
-func (e DecodingError) Error() string {
-	return "decoding error: " + strings.Join(e.Reasons, "; ")
-}
-
-type NoHandler struct {
-	Message serverMessage.Message
-}
-
-func (e NoHandler) Error() string {
-	return fmt.Sprintf("no handler for given message %s", e.Message)
-}
-
-type MessageConversion struct {
-	Reasons []string
-}
-
-func (e MessageConversion) Error() string {
-	return "error converting message: " + strings.Join(e.Reasons, "; ")
-}
-
-type SendingMessage struct {
-	Reasons []string
-	Message serverMessage.Message
-}
-
-func (e SendingMessage) Error() string {
-	return fmt.Sprintf("error sending message: %s : %s", e.Message, strings.Join(e.Reasons, "; "))
 }
